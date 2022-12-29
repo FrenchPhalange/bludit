ede31a1be1e461237f4b957075f7672caeb0ac84
Romain Cian <romain.cian@kalixys.com>
2022-12-29 10:08:31 +0100

    fr gitlab

---
+++ bl-content/databases/plugins/maintenance-mode/db.php
@@ -0,0 +1,2 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+{"enable":false,"message":"l'Admin fait des carabistouilles, donc le blog est temporairement hors service...\u26a0\ufe0f","position":1}
